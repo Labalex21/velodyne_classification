cb4e73117a0ef0e0a6d27749c099401232928008
Labalex21 <labalex21@gmail.com>
2018-04-05 12:21:17 +0200

    Update dynamic_detection_velodyne.py

--- dynamic_detection_velodyne.py
+++ dynamic_detection_velodyne.py
@@ -28,11 +28,11 @@ dir_labels_training = dir_data + "labels_training/"
 dir_imgs_testing = dir_data + "testing/"
 dir_records = dir_data + "records/"
 #path_model = "D:/Velodyne/20180201_icsens_innenstadt/models/conv_dyn_velodyne.ckpt"
-path_model = "../data/models/conv_dyn_velodyne.ckpt"
+path_model = "../data/models/conv_dyn_velodyne_50.ckpt"
 
 # input data parameters
-epochs = 100
-batch_size = 100
+epochs = 200
+batch_size = 50
 
 # images parameters
 max_dist = 40
